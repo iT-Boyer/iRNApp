7d0a86105e5c14d60e6a0ba1ccabae905766a7ab
huos3203 <724987481@qq.com>
2018-11-24 15:34:37 +0800

    更新

--- App.js
+++ App.js
@@ -7,7 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+///导入图片控件
+import {Platform, Image, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +17,24 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+var MOCKED_MOVIES_DATA = [{
+  title:"标题",
+  year:"2015",
+  posters:{thumbnail:"https://i.imgur.com/UePbdph.jpg"}
+}];
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+    var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>第一个APP 完成!</Text>
+        <Text style={styles.instructions}>编辑 App.js 更新UI</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Text>{movie.title}</Text>
+        <Text>{movie.year}</Text>
+        <Text>{movie.posters.thumbnail }</Text>
+        <Image source = {{ uri: movie.posters.thumbnail }} style={styles.thumbnail}/>
       </View>
     );
   }
@@ -45,5 +56,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  thumbnail:{
+    width: 53,
+    height: 81
   },
 });
